12c68fe78a36f1749118731b2e4975270f161f04
Joel <yantornojoel@gmail.com>
2020-09-30 20:51:06 -0300

    Version 1

---
+++ docs/precache-manifest.8acfb10cf8455ca6f8d063340232048c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69a5e9d494082f6076eb03105af781c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d401e78875e2cd5b76fa",
+    "url": "/static/css/main.3c33840d.chunk.css"
+  },
+  {
+    "revision": "58b0b33b58349818feb2",
+    "url": "/static/js/2.1246b314.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1246b314.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d401e78875e2cd5b76fa",
+    "url": "/static/js/main.d1572eb5.chunk.js"
+  },
+  {
+    "revision": "3c74ffc820bc4e602115",
+    "url": "/static/js/runtime-main.e87ef1f7.js"
+  }
+]);
